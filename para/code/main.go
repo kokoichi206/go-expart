15e6d63de85471afea8ec8af3ee38589d3888418
takahiro.tominaga <tt.300607@gmail.com>
2022-03-06 01:46:18 +0900

    sec 3

--- para/code/main.go
+++ para/code/main.go
@@ -10,4 +10,24 @@ func main() {
 	if data == 0 {
 		fmt.Printf("the data is %v.\n", data)
 	}
+	
+	// intStream := make(chan int)
+	// close(intStream)
+	// // 閉じたチャネルからも読み取れる
+	// integer, ok := <-intStream
+	// fmt.Printf("(%v): %v\n", ok, integer)
+
+	intStream := make(chan int)
+	go func() {
+		defer close(intStream)
+
+		for i := 1; i <= 5; i++ {
+			intStream <- i
+		}
+	}()
+
+	// チャネルを引数にとり、チャネルが閉じた時に自動的にループを終了する
+	for integer := range intStream {
+		fmt.Printf("%v \n", integer)
+	}
 }
