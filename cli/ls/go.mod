95ac5682715c88ddd1b33f7d6f9b454f64f868b4
takahiro.tominaga <tt.300607@gmail.com>
2022-07-26 00:12:09 +0900

    color and help flag

--- cli/ls/go.mod
+++ cli/ls/go.mod
@@ -2,4 +2,7 @@ module ls
 
 go 1.16
 
-require github.com/spf13/pflag v1.0.5 // indirect
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- cli/ls/main.go
+++ cli/ls/main.go
@@ -2,31 +2,89 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"io/ioutil"
-	"log"
+	"os"
 
+	"github.com/fatih/color"
 	flag "github.com/spf13/pflag"
 )
 
-func main() {
+type Params struct {
+	IsHelp  bool
+	IsColor bool
+	Args    []string
+	Name    string
+}
+
+// Print usage
+var Usage = func() {
+	fmt.Println("NAME")
+	fmt.Printf("\tls – list directory contents\n\n")
+	fmt.Printf("The following options are available:\n")
+	flag.PrintDefaults()
+}
+
+// Is this global var ok?
+var params Params
+
+func init() {
+	flag.BoolVarP(&params.IsHelp, "help", "h", false, "Print help message")
+	flag.BoolVarP(&params.IsColor, "G", "G", false, "Print with bold cyan")
+
 	flag.Parse()
-	args := flag.Args()
-	var target string
-	if len(args) == 0 {
-		target = "."
-	} else if len(args) == 1 {
-		target = args[0]
-	} else {
-		// TODO: Multiple arguments output
-		return
-	}
-	files, err := ioutil.ReadDir(target)
-	if err != nil {
-		log.Fatal(err)
+
+	params.Args = flag.Args()
+}
+
+func main() {
+
+	output(params)
+}
+
+func output(params Params) {
+	if params.IsHelp {
+		Usage()
+		os.Exit(0)
 	}
 
+	if len(params.Args) == 0 {
+		params.Name = "."
+	} else if len(params.Args) == 1 {
+		params.Name = params.Args[0]
+	} else {
+		// TODO: Multiple arguments output
+		os.Exit(0)
+	}
+
+	files, err := ioutil.ReadDir(params.Name)
+	if err != nil {
+		fmt.Printf("ls: %s: No such file or directory\n", params.Name)
+		os.Exit(1)
+	}
+
+	if params.IsColor {
+		printFilesWithColor(files)
+	} else {
+		printFiles(files)
+	}
+
+	fmt.Println()
+}
+
+func printFiles(files []fs.FileInfo) {
 	for _, file := range files {
 		fmt.Printf("%s\t", file.Name())
 	}
-	fmt.Println()
+}
+
+func printFilesWithColor(files []fs.FileInfo) {
+	for _, file := range files {
+		if file.IsDir() {
+			d := color.New(color.FgCyan, color.Bold)
+			d.Printf("%s\t", file.Name())
+		} else {
+			fmt.Printf("%s\t", file.Name())
+		}
+	}
 }
