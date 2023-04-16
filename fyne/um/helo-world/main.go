d8b5308dfed6ab27678c2c9b9eeb3d730c514ca4
takahiro.tominaga <tt.300607@gmail.com>
2023-04-16 18:29:57 +0900

    some change

--- fyne/um/helo-world/main.go
+++ fyne/um/helo-world/main.go
@@ -3,22 +3,50 @@ package main
 import (
 	"fmt"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+type App struct {
+	output *widget.Label
+}
+
+var myApp App
 
 func main() {
 	a := app.New()
 	// at least 1 window
 	w := a.NewWindow("Hello")
 
-	w.SetContent(widget.NewLabel("Hello Fyne!"))
+	output, entry, btn := myApp.makeUI()
+
+	w.SetContent(container.NewVBox(output, entry, btn))
+	w.Resize(fyne.Size{Width: 500, Height: 500})
+
+	// w.SetContent(widget.NewLabel("Hello Fyne!"))
 	// start event loop
 	w.ShowAndRun()
 	// w.Show()
 	// a.Run()
 
 	something()
+}
+
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+	output := widget.NewLabel("Hello world!")
+	entry := widget.NewEntry()
+	btn := widget.NewButton("Enter", func() {
+		app.output.SetText(entry.Text)
+	})
+
+	// also change color
+	btn.Importance = widget.HighImportance
+
+	app.output = output
+
+	return output, entry, btn
 }
 
 func something() {
