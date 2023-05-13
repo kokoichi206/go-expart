58085f5277b8dbc72c0db1784f36a4f1adfdcb0d
takahiro.tominaga <tt.300607@gmail.com>
2023-05-14 02:43:50 +0900

    feat: first evaluator

--- language/monkey/repl/repl.go
+++ language/monkey/repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey-language/evaluator"
 	"monkey-language/lexer"
 	"monkey-language/parser"
 )
@@ -46,8 +47,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
