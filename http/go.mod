7371172df87808038380a8a8dc877b9092076a3b
takahiro.tominaga <tt.300607@gmail.com>
2023-05-20 23:37:53 +0900

    WIP: ws proxy ?

--- http/go.mod
+++ http/go.mod
@@ -1,3 +1,14 @@
 module myhttp
 
 go 1.20
+
+require (
+	github.com/gobwas/ws v1.2.1
+	github.com/magiconair/properties v1.8.7
+)
+
+require (
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- http/proxy_test.go
+++ http/proxy_test.go
@@ -1,12 +1,18 @@
 package main_test
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+	"github.com/magiconair/properties/assert"
 )
 
 func TestProxy(t *testing.T) {
@@ -36,4 +42,33 @@ func TestProxy(t *testing.T) {
 	if string(body) != "hello" {
 		t.Error("a response is not hello")
 	}
+}
+
+func TestProxyWS(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		conn, _, _, err := ws.UpgradeHTTP(r, w)
+		if err != nil {
+			log.Fatal(err)
+
+			return
+		}
+
+		wsutil.WriteServerText(conn, []byte("hello"))
+
+		conn.Close()
+	}))
+	defer ts.Close()
+
+	// Proxy settings (used in client > Transport > proxy?)
+	os.Setenv("HTTP_PROXY", ts.URL)
+
+	conn, _, _, err := ws.Dial(context.Background(), "ws://my.domain.com:8930")
+	if err != nil {
+		log.Fatal(err)
+
+		return
+	}
+
+	b, err := wsutil.ReadServerText(conn)
+	assert.Equal(t, string(b), "hello")
 }
