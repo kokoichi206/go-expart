c102558a2054b9831c19ddeb3a9d66db6d70ccc3
takahiro.tominaga <tt.300607@gmail.com>
2023-05-23 23:59:34 +0900

    sse

--- sse/main.go
+++ sse/main.go
@@ -5,6 +5,7 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,9 +19,33 @@ func main() {
 	go currentGoroutine()
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"*",
+		},
+		AllowMethods: []string{
+			"GET",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
 
 	router.GET("/stream", addSseHeaders(), func(c *gin.Context) {
 		for range time.Tick(time.Second * 3) {
+			select {
+			case <-c.Request.Context().Done():
+				fmt.Println("client closed")
+			default:
+			}
 			now := time.Now().Format("2006-01-02 15:04:05")
 			currentTime := fmt.Sprintf("The Current Time Is %v", now)
 
@@ -41,6 +66,7 @@ func addSseHeaders() gin.HandlerFunc {
 		c.Writer.Header().Set("Cache-Control", "no-cache")
 		c.Writer.Header().Set("Connection", "keep-alive")
 		c.Writer.Header().Set("Transfer-Encoding", "chunked")
+		c.Writer.Header().Set("X-Accel-Buffering", "no")
 		c.Next()
 	}
 }
---
+++ sse/memo.md
@@ -0,0 +1,15 @@
+## websocket 接続してサーバーしてサーバー側を切断したとき
+
+onerror のコールバックが呼ばれる、その時の Event 情報は以下　
+
+![](imgs/server_error.png)
+
+### [mdm: error event](https://developer.mozilla.org/ja/docs/Web/API/EventSource/error_event)
+
+readyState
+
+### [イベントストリームを閉じる](https://developer.mozilla.org/ja/docs/Web/API/Server-sent_events/Using_server-sent_events#%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E3%82%B9%E3%83%88%E3%83%AA%E3%83%BC%E3%83%A0%E3%82%92%E9%96%89%E3%81%98%E3%82%8B)
+
+> 既定では、クライアントとサーバーの間の接続を閉じると、接続が再起動します。
+> 接続は .close() メソッドで終了します。
+
