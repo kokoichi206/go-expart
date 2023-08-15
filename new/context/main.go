9dc115a9adb43dbe60d1d3ef23f62563af6e2a77
takahiro.tominaga <tt.300607@gmail.com>
2023-08-16 00:34:39 +0900

    1.21 context AfterFunc

--- new/context/main.go
+++ new/context/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -24,5 +26,29 @@ func handlerWithout(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/handler", handler)
 	http.HandleFunc("/handler1", handlerWithout)
-	http.ListenAndServe(":8818", nil)
+	go http.ListenAndServe(":8818", nil)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	stopc := make(chan struct{})
+	stop := context.AfterFunc(ctx, func() {
+		fmt.Println("stop")
+		close(stopc)
+	})
+	time.Sleep(2 * time.Second)
+
+	// stop を呼ぶことで、引数に渡した関数を実行する！
+	// ctx.Done がそれより前に呼ばれていた場合、stop は false を返し、関数も実行されない！
+	if !stop() {
+		fmt.Println("waiting stopc...!")
+
+		// wait for it's completion!
+		<-stopc
+		fmt.Println("done!")
+	}
+	fmt.Println("finished!")
+
+	fmt.Printf("errors.ErrUnsupported: %v\n", errors.ErrUnsupported)
 }
