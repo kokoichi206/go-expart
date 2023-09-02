b56aff1e6a909eac57c6dbb8c1714b742ea0dc7a
takahiro.tominaga <tt.300607@gmail.com>
2023-09-03 01:41:20 +0900

    gs standard

--- server/graceful/main.go
+++ server/graceful/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -8,16 +9,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	fmt.Fprintf(w, "Hello, World")
+}
+
+func standardServer() {
+	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.ListenAndServe(":8080", nil)
+}
+
+func ginServer() {
 	engine := gin.Default()
-	engine.Handle(http.MethodGet, "/hello", func(context *gin.Context) {
+	engine.Handle(http.MethodGet, "/gin", func(context *gin.Context) {
 		time.Sleep(5 * time.Second)
 		context.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
+			"message": "Hello World Gin",
 		})
 	})
 
 	if err := engine.Run(":21829"); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	standardServer()
+
+	// ginServer()
 }
