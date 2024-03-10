6dc6ed12f49cc289135b6067e4edcb9868ab168f
kokoichi206 <tt.300607@gmail.com>
2024-03-10 22:59:38 +0900

    create presigned URL for R2

--- anything/drive-to-s3/README.md
+++ anything/drive-to-s3/README.md
@@ -4,3 +4,4 @@
 - [Unseekable Streaming Input](https://aws.github.io/aws-sdk-go-v2/docs/sdk-utilities/s3/#unseekable-streaming-input)
 - [署名付き URL を使用したオブジェクトの共有](https://docs.aws.amazon.com/ja_jp/AmazonS3/latest/userguide/ShareObjectPreSignedURL.html)
 - [Create a presigned URL for Amazon S3 using an AWS SDK](https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_Scenario_PresignedUrl_section.html)
+- [R2: Presigned URLs](https://developers.cloudflare.com/r2/api/s3/presigned-urls/)
--- anything/drive-to-s3/main.go
+++ anything/drive-to-s3/main.go
@@ -16,6 +16,56 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+func cfClient() *s3.Client {
+	accountID := os.Getenv("ACCOUNT_ID")
+	endpoint := fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountID)
+
+	resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, opts ...interface{}) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			URL:               endpoint,
+			HostnameImmutable: true,
+			SigningRegion:     region,
+		}, nil
+	})
+
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithRegion("auto"),
+		config.WithEndpointResolverWithOptions(resolver),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// **aws の** s3 client を作成する。
+	client := s3.NewFromConfig(cfg)
+
+	return client
+}
+
+func cfPreSignedURL() string {
+	client := cfClient()
+
+	pc := s3.NewPresignClient(
+		client,
+		// 10 秒で期限切れになる署名付き URL とする。
+		// default は 15 分 (900 秒)。
+		s3.WithPresignExpires(10*time.Second),
+	)
+
+	req, err := pc.PresignGetObject(context.Background(), &s3.GetObjectInput{
+		Bucket: aws.String("my-first-bucket"),
+		Key:    aws.String("aya2.png"),
+	})
+
+	if err != nil {
+		return ""
+	}
+
+	fmt.Printf("req: %v\n", req)
+	return req.URL
+}
+
 // https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_Scenario_PresignedUrl_section.html
 func preSignedURL(ctx context.Context, objectName string) string {
 	bucket := os.Getenv("S3_BUCKET")
@@ -98,6 +148,8 @@ func fetchAndUpload(ctx context.Context, viewURL string, fileName string) {
 }
 
 func main() {
+	fmt.Printf("cfPreSignedURL(): %v\n", cfPreSignedURL())
+	return
 	ctx := context.Background()
 	preSignedURL(ctx, "nanka.pdf")
 	return
