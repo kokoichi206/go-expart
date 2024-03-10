e14964d726eb17ff9287736e1225deda886c4bbc
kokoichi206 <tt.300607@gmail.com>
2024-03-10 20:23:02 +0900

    create presigned URL

--- anything/drive-to-s3/main.go
+++ anything/drive-to-s3/main.go
@@ -8,12 +8,44 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+// https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_Scenario_PresignedUrl_section.html
+func preSignedURL(ctx context.Context, objectName string) string {
+	bucket := os.Getenv("S3_BUCKET")
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	pc := s3.NewPresignClient(
+		client,
+		// 10 秒で期限切れになる署名付き URL とする。
+		// default は 15 分 (900 秒)。
+		s3.WithPresignExpires(10*time.Second),
+	)
+
+	req, err := pc.PresignGetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(objectName),
+	})
+
+	if err != nil {
+		return ""
+	}
+
+	fmt.Printf("req: %v\n", req)
+	return req.URL
+}
 
 func upload(ctx context.Context, objectName string, object io.Reader, len int64) {
 	bucket := os.Getenv("S3_BUCKET")
@@ -62,10 +94,12 @@ func fetchAndUpload(ctx context.Context, viewURL string, fileName string) {
 
 	defer res.Body.Close()
 
-	upload(ctx, "nanka.pdf", res.Body, res.ContentLength)
+	upload(ctx, fileName, res.Body, res.ContentLength)
 }
 
 func main() {
 	ctx := context.Background()
+	preSignedURL(ctx, "nanka.pdf")
+	return
 	fetchAndUpload(ctx, os.Getenv("DRIVE_URL"), "nanka.pdf")
 }
