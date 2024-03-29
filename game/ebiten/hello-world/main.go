a5d563d5be4cdd9c937b82533c95d862e6f5f56a
kokoichi206 <tt.300607@gmail.com>
2024-03-30 05:19:34 +0900

    [feat] 敵を追加する

--- game/ebiten/hello-world/main.go
+++ game/ebiten/hello-world/main.go
@@ -8,6 +8,7 @@ import (
 	"image/color"
 	_ "image/png"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -17,16 +18,31 @@ import (
 const (
 	screenWidth  = 640
 	screenHeight = 480
-	catSize      = 64
-	catSpeed     = 4
+
+	catImgSize = 64
+	catSize    = 48 // 当たり判定に使う、実際の猫のサイズ。
+	catSpeed   = 4
+
+	enemyImgSize = 64
+	enemySize    = 48 // 当たり判定に使う、実際の敵のサイズ。
+	snakeSpeed   = 1.5
 )
 
 var (
 	//go:embed cat.png
 	Cat_png []byte
 
-	catImage *ebiten.Image
-	bg       *ebiten.Image
+	//go:embed snake.png
+	Snake_png []byte
+
+	//go:embed gameover.png
+	Gameover_png []byte
+
+	catImage        *ebiten.Image
+	enemySnakeImage *ebiten.Image
+
+	bg           *ebiten.Image
+	gameoverLogo *ebiten.Image
 )
 
 func init() {
@@ -34,31 +50,35 @@ func init() {
 	if err != nil {
 		log.Fatal("image.Decode error: ", err)
 	}
+	catImage = ebiten.NewImageFromImage(img)
+
+	img, _, err = image.Decode(bytes.NewReader(Snake_png))
+	if err != nil {
+		log.Fatal("image.Decode error: ", err)
+	}
+	enemySnakeImage = ebiten.NewImageFromImage(img)
+
+	img, _, err = image.Decode(bytes.NewReader(Gameover_png))
+	if err != nil {
+		log.Fatal("image.Decode error: ", err)
+	}
+	gameoverLogo = ebiten.NewImageFromImage(img)
 
 	bg = ebiten.NewImage(screenWidth, screenHeight)
 	vector.DrawFilledRect(
 		bg, 0, 0, screenWidth, screenHeight,
 		color.RGBA{0, 0xff, 0, 0xff}, true)
-
-	catImage = ebiten.NewImageFromImage(img)
 }
 
 type Game struct {
-	cat *Cat
+	cat   *Cat
+	snake *Snake
 }
 
 func NewGame() *Game {
 	return &Game{
-		cat: &Cat{
-			pos: Position{
-				X: screenWidth / 3,
-				Y: catSize / 2,
-			},
-			vec: Velocity{
-				X: 0,
-				Y: 0,
-			},
-		},
+		cat:   NewCat(),
+		snake: NewSnake(),
 	}
 }
 
@@ -66,6 +86,19 @@ type Cat struct {
 	// 中心のポジション。
 	pos Position
 	vec Velocity
+}
+
+func NewCat() *Cat {
+	return &Cat{
+		pos: Position{
+			X: screenWidth / 3,
+			Y: catImgSize / 2,
+		},
+		vec: Velocity{
+			X: 0,
+			Y: 0,
+		},
+	}
 }
 
 func (c *Cat) update() {
@@ -81,9 +114,9 @@ func (c *Cat) update() {
 	}
 
 	// 地面より下に行かないようにする。
-	if c.pos.Y+c.vec.Y <= catSize/2 {
+	if c.pos.Y+c.vec.Y <= catImgSize/2 {
 		c.vec.Y = 0
-		c.pos.Y = catSize / 2
+		c.pos.Y = catImgSize / 2
 	} else {
 		c.pos.Y += c.vec.Y
 		c.vec.Y -= 0.1
@@ -92,8 +125,37 @@ func (c *Cat) update() {
 
 func (c *Cat) draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(float64(c.pos.X-catSize/2), float64(screenHeight-(c.pos.Y+catSize/2)))
+	op.GeoM.Translate(float64(c.pos.X-catImgSize/2), float64(screenHeight-(c.pos.Y+catImgSize/2)))
 	screen.DrawImage(catImage, op)
+}
+
+type Snake struct {
+	// 中心のポジション。
+	pos Position
+	vec Velocity
+}
+
+func (s *Snake) update() {
+	s.pos.X -= snakeSpeed
+}
+
+func (s *Snake) draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(s.pos.X-enemyImgSize/2), float64(screenHeight-(s.pos.Y+enemyImgSize/2)))
+	screen.DrawImage(enemySnakeImage, op)
+}
+
+func NewSnake() *Snake {
+	return &Snake{
+		pos: Position{
+			X: screenWidth,
+			Y: enemyImgSize / 2,
+		},
+		vec: Velocity{
+			X: 0,
+			Y: 0,
+		},
+	}
 }
 
 type Position struct {
@@ -107,7 +169,10 @@ type Velocity struct {
 }
 
 func (g *Game) Update() error {
-	g.cat.update()
+	if !g.dead() {
+		g.cat.update()
+		g.snake.update()
+	}
 
 	return nil
 }
@@ -116,7 +181,20 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(bg, nil)
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("x, y, vx, vy: %.2f, %.2f, %.2f, %.2f", g.cat.pos.X, g.cat.pos.Y, g.cat.vec.X, g.cat.vec.Y))
+
 	g.cat.draw(screen)
+	g.snake.draw(screen)
+
+	if g.dead() {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(screenWidth/2-float64(gameoverLogo.Bounds().Dx())/2, screenHeight/2-float64(gameoverLogo.Bounds().Dy())/2)
+		screen.DrawImage(gameoverLogo, op)
+	}
+}
+
+func (g *Game) dead() bool {
+	// 球体とみなして当たり判定を行う。
+	return math.Pow(g.cat.pos.X-g.snake.pos.X, 2)+math.Pow(g.cat.pos.Y-g.snake.pos.Y, 2) < math.Pow(catSize/2+enemySize/2, 2)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
