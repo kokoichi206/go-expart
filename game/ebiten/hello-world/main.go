7f78d2aed69a474d0650762a618b2195fb6a6e3c
kokoichi206 <tt.300607@gmail.com>
2024-03-30 04:23:14 +0900

    [feat] 猫様を動かさせてもらう

--- game/ebiten/hello-world/main.go
+++ game/ebiten/hello-world/main.go
@@ -18,6 +18,7 @@ const (
 	screenWidth  = 640
 	screenHeight = 480
 	catSize      = 64
+	catSpeed     = 4
 )
 
 var (
@@ -27,37 +28,6 @@ var (
 	catImage *ebiten.Image
 	bg       *ebiten.Image
 )
-
-type Game struct {
-	cat *Cat
-}
-
-func NewGame() *Game {
-	return &Game{
-		cat: &Cat{
-			pos: Position{
-				X: screenWidth / 3,
-				Y: catSize / 2,
-			},
-		},
-	}
-}
-
-type Cat struct {
-	// 中心のポジション。
-	pos Position
-}
-
-func (c *Cat) draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(float64(c.pos.X-catSize/2), float64(screenHeight-(c.pos.Y+catSize/2)))
-	screen.DrawImage(catImage, op)
-}
-
-type Position struct {
-	X int
-	Y int
-}
 
 func init() {
 	img, _, err := image.Decode(bytes.NewReader(Cat_png))
@@ -73,15 +43,79 @@ func init() {
 	catImage = ebiten.NewImageFromImage(img)
 }
 
+type Game struct {
+	cat *Cat
+}
+
+func NewGame() *Game {
+	return &Game{
+		cat: &Cat{
+			pos: Position{
+				X: screenWidth / 3,
+				Y: catSize / 2,
+			},
+			vec: Velocity{
+				X: 0,
+				Y: 0,
+			},
+		},
+	}
+}
+
+type Cat struct {
+	// 中心のポジション。
+	pos Position
+	vec Velocity
+}
+
+func (c *Cat) update() {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		c.pos.X -= catSpeed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		c.pos.X += catSpeed
+	}
+	if (ebiten.IsKeyPressed(ebiten.KeyArrowUp) || ebiten.IsKeyPressed(ebiten.KeySpace)) &&
+		c.vec.Y == 0 {
+		c.vec.Y += 4
+	}
+
+	// 地面より下に行かないようにする。
+	if c.pos.Y+c.vec.Y <= catSize/2 {
+		c.vec.Y = 0
+		c.pos.Y = catSize / 2
+	} else {
+		c.pos.Y += c.vec.Y
+		c.vec.Y -= 0.1
+	}
+}
+
+func (c *Cat) draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(c.pos.X-catSize/2), float64(screenHeight-(c.pos.Y+catSize/2)))
+	screen.DrawImage(catImage, op)
+}
+
+type Position struct {
+	X float64
+	Y float64
+}
+
+type Velocity struct {
+	X float64
+	Y float64
+}
+
 func (g *Game) Update() error {
+	g.cat.update()
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("x, y: %d, %d", g.cat.pos.X, g.cat.pos.Y))
-
 	screen.DrawImage(bg, nil)
 
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("x, y, vx, vy: %.2f, %.2f, %.2f, %.2f", g.cat.pos.X, g.cat.pos.Y, g.cat.vec.X, g.cat.vec.Y))
 	g.cat.draw(screen)
 }
 
