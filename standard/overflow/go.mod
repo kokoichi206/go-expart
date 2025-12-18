eb6958435cb8276d3a43e426d2e527b3c634b3da
TAKAHiRO TOMiNAGA <52474650+kokoichi206@users.noreply.github.com>
2025-12-18 23:36:40 +0900

    chore: add overflow test

---
+++ standard/overflow/go.mod
@@ -0,0 +1,3 @@
+module github.com/kokoichi206/go-expart/standard/overflow
+
+go 1.23.3
---
+++ standard/overflow/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var maxInt int32 = math.MaxInt32
+	fmt.Println(maxInt)
+	fmt.Println(maxInt + 1)
+}
